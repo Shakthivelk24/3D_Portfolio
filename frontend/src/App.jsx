5d7b5ae327372c5e9af440d86ffe7c6331c63add
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-06 22:22:49 +0530

    Updated the App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,6 @@ const App = () => {
         <About />
         <Tech />
         <Works />
-        <Feedbacks />
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
